5593593a4ac86a870de0f4535024c6b1007a9cdd
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2020-08-24 09:58:39 +0530

    intial commit

--- index.js
+++ index.js
@@ -26,5 +26,17 @@ $('#email').on('click',function()
 	{
 		$('.mail').css({"background-color":"rgba(255,255,255,0.3)","font-size":"1.3rem"})
 		$('.linkedin').css({"background-color":"transparent","font-size":"1em"});
-		
+
 	});
+if($(window).width()<1000)
+{
+	$('.background').attr('src','b2.png');
+	$('#contact').css("font-size","1.7rem");
+	$('.name').css("font-size","2.5rem");
+	$('nav').css("font-size",'2.5rem');
+	$('.dropdown-item > img').css("width",'80');
+	$('.card-title').css("font-size","2rem")
+	$('.card-text').css("font-size","1.8rem")
+	$('#scan').remove();
+
+}
